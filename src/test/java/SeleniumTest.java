20f3a7686cc60f7d4e158652c7252b8ab0d99250
Zulumuly <nataplaxina@gmail.com>
2024-11-05 18:44:59 +0300

    Mini test for UI

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class SeleniumTest {
+
+    private WebDriver driver;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new SafariDriver();
+    }
+
+    @Test
+    public void openPageTest() {
+        driver.get("http://uitestingplayground.com/home");
+        String expectedTitle = "UI Test Automation Playground";
+        String actualTitle = driver.getTitle();
+        assertEquals(expectedTitle, actualTitle);
+    }
+
+    @Test
+    public void timeOutTest(){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5));
+        driver.get("http://uitestingplayground.com/home");
+    }
+
+    @Test
+    public void sizeOfSiteTest(){
+        driver.manage().window().maximize();
+        driver.manage().window().minimize();
+        driver.manage().window().fullscreen();
+    }
+
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
